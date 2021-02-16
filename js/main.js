8b3e147584ac9edea1f43f231154ab39a3491ba5
khalidjambi <khalid.jambi@gmail.comm>
2021-02-16 08:17:16 +0700

    halo

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// console.log('Hello World');
+
+// document.getElementById('text').innerHTML = 'Selamat pagi';
+
+
+
+// alert('Selamat pagii'); ///Dialog box alert
+
+
+
+// Dialog Box prompt
+// console.log('Baledemy');
+
+// document.getElementById('text').innerHTML = 'Selamat pagi';
+
+
+// prompt('Silahkan isi nama anda: ');
+
+
+// Dialog box Confirm
+console.log(prompt('Data Anda'));
+
+document.getElementById('text').innerHTML = '';
+
+confirm('Apa anda ingin menghapus?')
